3fbeea9e8925a0ed1491d3d09d491ac1f27a7e01
Ozjq <13506619546@163.com>
2025-05-20 15:04:41 +0800

    MCP服务端开发

---
+++ ai-image-search-mcp-server/src/test/java/com/zjq/aiimagesearchmcpserver/tools/ImageSearchToolTest.java
@@ -0,0 +1,19 @@
+package com.zjq.aiimagesearchmcpserver.tools;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ImageSearchToolTest {
+
+    @Resource
+    private ImageSearchTool imageSearchTool;
+    @Test
+    void searchImage() {
+        String res = imageSearchTool.searchImage("computer");
+        Assertions.assertNotNull(res);
+    }
+}
